0e4690981f06dc68f361753db0da948e3dbdf37c
Steven Yang <xxgw@sina.com>
2017-11-29 23:33:22 +0800

    list all jobs and import HSQL to store data

---
+++ src/main/java/com/ccblife/fiveonejob/Job.java
@@ -0,0 +1,33 @@
+package com.ccblife.fiveonejob;
+
+public class Job {
+
+	private String title;
+	private String company;
+	private String location;
+	private String salary;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+}
--- src/main/java/com/ccblife/fiveonejob/SalaryTest.java
+++ src/main/java/com/ccblife/fiveonejob/SalaryTest.java
@@ -1,7 +1,9 @@
 package com.ccblife.fiveonejob;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,19 +15,32 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 public class SalaryTest {
+	private NamedParameterJdbcTemplate jdbcTemplate;
 	private WebDriver driver;
+	private WebDriverWait wait;
 	private String baseUrl;
-	private String nextId = "#rtNext .on";
-	private String jobsPerPageCss  = "#resultList .el .t1 a";
-	private String titleCss = "#resultList .el .t1 a:nth(0)";
+	private String nextCss = "#rtNext.dicon.Dm.on";
+	private String jobsPerPageCss  = "#resultList .el:not(.title)";
+	private String idCss = ".t1 input";
+	private String titleCss = ".t1 a";
+	private String companyCss = ".t2 a";
+	private String locationCss = ".t1 a";
+	private String salaryCss = ".t1 a";
 //	private List<Job> jobs = new ArrayList();
 
 	@Before
 	public void setUp() throws Exception {
+		//prepare the database
+		jdbcTemplate = jdbcTemplate();
 		System.setProperty("webdriver.chrome.driver", "D:\\Program Files\\selenium\\chromedriver.exe");
 		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, 6);
 		driver.manage().window().maximize();
 		baseUrl = "http://51job.com";
 		driver.get(baseUrl);
@@ -36,18 +51,33 @@ public class SalaryTest {
 		driver.findElement(By.id("kwdselectid")).clear();
 		driver.findElement(By.id("kwdselectid")).sendKeys("¼Ü¹¹Ê¦");
 		driver.findElement(By.id("kwdselectid")).sendKeys(Keys.ENTER);
-
-		WebDriverWait wait = new WebDriverWait(driver, 6);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(jobsPerPageCss)));
 		
 		do {
 			List<WebElement> jobWebElements = driver.findElements(By.cssSelector(jobsPerPageCss));
 			for (WebElement jobWebElement : jobWebElements) {
-//				WebElement title = driver.findElement(By.cssSelector(titleCss));
-//				System.out.println(title.getText());
+
+				String id = jobWebElement.findElement(By.cssSelector(idCss)).getAttribute("value");
+				String title = jobWebElement.findElement(By.cssSelector(titleCss)).getText();
+				String company = jobWebElement.findElement(By.cssSelector(companyCss)).getText();
+				String location = jobWebElement.findElement(By.cssSelector(locationCss)).getText();
+				int salary = Integer.valueOf(jobWebElement.findElement(By.cssSelector(salaryCss)).getText());
+				
+				Map<String, Object> params = new HashMap<>();
+				params.put("id", id);
+				params.put("title", title);
+				params.put("company", company);
+				params.put("location", location);
+				params.put("salary", salary);
+				
+				String SQL = "insert into job (id, title, company, location, salary) values (?,?,?,?,?)";
+				jdbcTemplate.update(SQL, params);
+
+				System.out.println(id + title + company + location + salary);
 			}
 			if (hasNext()){
-				driver.findElement(By.id(nextId)).click();
-				wait.until(ExpectedConditions.presenceOfElementLocated(By.id(nextId)));
+				driver.findElement(By.cssSelector(nextCss)).click();
+				wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(nextCss)));
 			}
 			else
 				break;
@@ -56,10 +86,19 @@ public class SalaryTest {
 
 	public boolean hasNext() {
 		try {
-			WebElement nextWebElement = driver.findElement(By.id(nextId));
+			driver.findElement(By.cssSelector(nextCss));
 			return true;
 		} catch (NoSuchElementException e) {
 			return false;
 		}
+	}
+	
+	public NamedParameterJdbcTemplate jdbcTemplate(){
+		EmbeddedDatabaseBuilder  builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder
+							  .setType(EmbeddedDatabaseType.HSQL)
+							  .addScript("db/sql/create-table.sql")
+							  .build();
+		return new NamedParameterJdbcTemplate(db);
 	}
 }
